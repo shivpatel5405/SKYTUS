2d87fe31e4d4fae284f67e365a2eafa5c076fe55
shivpatel5405 <shivpatel50405@gmail.com>
2026-09-03 15:31:28 +0530

    Task_06 added

---
+++ Python/Operators & Expressions/Task_06.py
@@ -0,0 +1,9 @@
+# Take two numbers and print true if both are positive else false
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+if(num1 > 0 and num2 > 0):
+    print("True")
+else:
+    print("False")
+    
