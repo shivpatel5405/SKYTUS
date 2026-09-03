ca2b72f4de2d89121df14d6da8479c4054bc254d
shivpatel5405 <shivpatel50405@gmail.com>
2026-09-03 15:58:02 +0530

    string to number and multiply by 10

---
+++ Python/Operators & Expressions/Task_08.py
@@ -0,0 +1,5 @@
+# Take a number as string , convert to int , and multiply by 10
+number = input("Enter a number: ")
+convert_num = int(number)
+result = convert_num * 10
+print("The result of multiplying the number by 10 is:", result)
