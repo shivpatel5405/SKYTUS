c46c52b280a4ef3df21e66d964248a3af48d5b25
shivpatel5405 <shivpatel50405@gmail.com>
2026-09-03 16:09:11 +0530

    print the quotient and remainder seperately

---
+++ Python/Operators & Expressions/Task_10.py
@@ -0,0 +1,7 @@
+# Divide two numbers and print the quotient and remainder seperately
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+quotient = num1 // num2
+remainder = num1 % num2
+print("The quotient is:", quotient)
+print("The remainder is:", remainder)
