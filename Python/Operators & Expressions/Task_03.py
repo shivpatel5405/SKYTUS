0cdfa65cd2444a9c2519f0dcaccb5aef2df88aa2
shivpatel5405 <shivpatel50405@gmail.com>
2026-09-04 10:42:36 +0530

    compare two numbers

--- Python/Operators & Expressions/Task_03.py
+++ Python/Operators & Expressions/Task_03.py
@@ -4,8 +4,8 @@ num1=int(input("Enter the number-1 : "))
 num2=int(input("Enter the number-2 : "))
 
 if num1>num2:
-    print(f"{num1} is the greater number")
+    print(num1, "is the greater number")
 elif num2>num1:
-    print(f"{num2} is the greater number")
+    print(num2, "is the greater number")
 else:
     print("Both numbers are equal")
