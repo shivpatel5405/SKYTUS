9b5e564dbca2d504c14d4387749ace26d1c70699
shivpatel5405 <shivpatel50405@gmail.com>
2026-09-04 10:28:56 +0530

    Celsius to Fahrenhiet

--- Python/Python Setup & Basics/Task_03.py
+++ Python/Python Setup & Basics/Task_03.py
@@ -2,7 +2,7 @@
 
 celsius = float(input("Enter temperature in Celsius : "))
 
-fahrenheit = (celsius * 9/5) + 32
+Fahrenheit = (celsius * 9/5) + 32
 
-print("Temperature in Fahrenheit is :", fahrenheit)
+print("Temperature in Fahrenheit is :", Fahrenheit)
 
