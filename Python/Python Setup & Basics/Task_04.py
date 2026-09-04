9af01d122d245755d3b9b011adad759b404ae450
shivpatel5405 <shivpatel50405@gmail.com>
2026-09-04 10:34:37 +0530

    program for printing name in uppercase

--- Python/Python Setup & Basics/Task_04.py
+++ Python/Python Setup & Basics/Task_04.py
@@ -1,4 +1,5 @@
 # Store your name in a variable and print it in uppercase
 
 name = input("Enter your name : ")
-print(name.upper())
+
+print(name.upper()) #python function to convert string to uppercase
