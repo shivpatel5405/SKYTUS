71f62930d1a262cefa6e9e2c9b63ebf3d826706e
shivpatel5405 <shivpatel50405@gmail.com>
2026-09-08 12:15:51 +0530

    Add triangle type checker program for equilateral, isosceles, and scalene triangles

---
+++ Python/Python Conditional Statements/Task_10.py
@@ -0,0 +1,12 @@
+# Check type of Triangle (Equilateral, Isosceles, or Scalene)
+side1 = float(input("Enter the length of the first side: "))
+side2 = float(input("Enter the length of the second side: "))
+side3 = float(input("Enter the length of the third side: "))
+
+if side1 == side2 == side3:
+    print("The triangle is equilateral.")
+elif side1 == side2 or side2 == side3 or side1 == side3:
+    print("The triangle is isosceles.")
+else:
+    print("The triangle is scalene.")
+    
