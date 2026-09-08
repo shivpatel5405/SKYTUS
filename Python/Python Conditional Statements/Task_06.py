3d6d7b1a581d7d93077bb4f2e746e75768926a14
shivpatel5405 <shivpatel50405@gmail.com>
2026-09-08 11:48:48 +0530

    Add range check program

---
+++ Python/Python Conditional Statements/Task_06.py
@@ -0,0 +1,7 @@
+# Check if the number lies within the given range
+number = float(input("Enter a number: "))
+if 10 <= number <= 20:
+    print(f"The number {number} lies within the range of 10 to 20.")
+else:
+    print(f"The number {number} does not lie within the range of 10 to 20.")
+    
