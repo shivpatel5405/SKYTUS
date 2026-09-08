b403951643b97a27c3a176910ef7cc43efe8f856
shivpatel5405 <shivpatel50405@gmail.com>
2026-09-08 10:47:19 +0530

    Add eligibility check for voting based on age

---
+++ Python/Python Conditional Statements/Task_01.py
@@ -0,0 +1,6 @@
+# Check if a person is eligible to vote (age >= 18) 
+age = int(input("Enter the age of the person: "))
+if age >= 18:
+    print("The person is eligible to vote.")
+else:
+    print("The person is not eligible to vote.")
