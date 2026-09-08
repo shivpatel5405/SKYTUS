9db8305cb10afb56ecbd06ede7cb0c7846210b5a
shivpatel5405 <shivpatel50405@gmail.com>
2026-09-08 10:59:26 +0530

    Add grade calculator based on marks

---
+++ Python/Python Conditional Statements/Task_02.py
@@ -0,0 +1,8 @@
+# Grade Calculator based on marks : 90+ = A, 80+ = B,else C
+marks = int(input("Enter your marks: "))
+if marks >= 90:
+    print("Your grade is A")
+elif marks>=80:
+    print("Your grade is B")
+else:
+    print("Your grade is C")
