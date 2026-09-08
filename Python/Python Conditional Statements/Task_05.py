680dd186e362e68f42e832fad31c115bf9bde332
shivpatel5405 <shivpatel50405@gmail.com>
2026-09-08 11:41:15 +0530

    Add number sign classification program

---
+++ Python/Python Conditional Statements/Task_05.py
@@ -0,0 +1,9 @@
+# check if the number is positive, negative or zero
+number = float(input("Enter a number: "))
+if number > 0:
+    print(f"The number {number} is positive.")
+elif number < 0:
+    print(f"The number {number} is negative.")
+else:
+    print(f"The number is zero.")
+    
