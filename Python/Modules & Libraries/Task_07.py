ace27ec1f6752ae76635c36340ae9b2492204e67
shivpatel5405 <shivpatel50405@gmail.com>
2026-09-23 10:34:28 +0530

    Added Task to Import multiple function from one module and use them

--- Python/Modules & Libraries/Task_07.py
+++ Python/Modules & Libraries/Task_07.py
@@ -2,13 +2,9 @@
 
 from maths import add, subtract, multiply
 
-
-# Get input from user
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
 
-
-# Use the imported functions
 print("Addition:", add(num1, num2))
 print("Subtraction:", subtract(num1, num2))
 print("Multiplication:", multiply(num1, num2))
